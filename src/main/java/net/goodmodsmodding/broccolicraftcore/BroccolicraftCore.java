d8941ec233aa788c332081242fa451d1d60d72d8
BeansBeefBroccoli <69123018+BeansBeefBroccoli@users.noreply.github.com>
2022-09-06 12:45:48 -0400

    whats wrong with my mods.toml

--- src/main/java/net/goodmodsmodding/broccolicraftcore/BroccolicraftCore.java
+++ src/main/java/net/goodmodsmodding/broccolicraftcore/BroccolicraftCore.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.goodmodsmodding.broccolicraftcore;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -17,15 +17,13 @@ import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
-{
+@Mod(BroccoliCraftCore.MOD_ID)
+public class BroccoliCraftCore {
+    public static final String MOD_ID = "tutorial";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
-    {
+    public BroccoliCraftCore() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -37,8 +35,7 @@ public class ExampleMod
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
@@ -47,7 +44,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("broccolicraftcore", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
